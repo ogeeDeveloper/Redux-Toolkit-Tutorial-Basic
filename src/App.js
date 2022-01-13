9de10e187515eadc265bfece9c991777e7eec016
ogeeDeveloper <omarogrant202@hotmail.com>
2022-01-13 12:16:31 -0500

    First Push debuging actions

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Auth from './components/Auth';
+import Counter from './components/Counter';
+import Header from './components/Header'
+import {useSelector} from 'react-redux'
+import UserProfile from './components/UserProfile'
+
+
+function App() {
+  const isAuth = useSelector(state => state.auth)
+  return (
+    <>
+      <Header/>
+      {!isAuth && <Auth /> }
+      {isAuth && <UserProfile/>}
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,44 @@
+import classes from './Counter.module.css';
+import {useSelector,useDispatch} from 'react-redux'
+import {counterAction} from '../store/index'
+
+const Counter = () => {
+  // This couter will be managed by redux
+  const counter = useSelector(state => state.counter.counter)
+  const show_counter = useSelector(state => state.counter.showCounter)
+
+  //Set up the Dispatch
+  const dispatch = useDispatch()
+
+  const toggleCounterHandler = () => {
+    dispatch(counterAction.toggle())
+  };
+
+  const incrementHandler=()=>{
+    dispatch(counterAction.increment())
+  }    
+  const decrementHandler=()=>{
+    dispatch(counterAction.decrement())
+  }  
+
+  const increaseHandler=()=>{
+    dispatch(counterAction.increase(10))
+  }
+
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show_counter && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
